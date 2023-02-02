b15765ffe6098b7c24205e7660d30355e83832c8
seojihee <90808075+diheet@users.noreply.github.com>
2023-02-02 21:43:59 +0900

    [level 0] Title: 약수 구하기, Time: 0.02 ms, Memory: 82.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] solution(int n) {
+        int cnt=0;
+        
+        for(int i=1;i<=n;i++){
+            if(n%i==0){
+                cnt++;
+            }
+        }
+        
+        int[] answer = new int[cnt];
+        int temp =0;
+        
+        for(int i=1;i<=n;i++){
+            if(n%i==0){
+                answer[temp] = i;
+                temp++;
+            }
+        }
+        return answer;
+    }
+}
