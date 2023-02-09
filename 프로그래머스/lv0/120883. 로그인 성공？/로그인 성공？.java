aa1e774175fc920c79890a4ab220e8bc7005a54a
seojihee <90808075+diheet@users.noreply.github.com>
2023-02-09 20:25:17 +0900

    [level 0] Title: 로그인 성공?, Time: 0.02 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String[] id_pw, String[][] db) {
+        String answer = "";
+        
+        for(int i=0; i<db.length;i++){
+            if(id_pw[0].equals(db[i][0]) && id_pw[1].equals(db[i][1])){
+                return answer = "login";
+            }else if(id_pw[0].equals(db[i][0]) && id_pw[1] != (db[i][1])){
+                return answer = "wrong pw";
+            }else answer = "fail";
+        }
+        return answer;
+    }
+}
